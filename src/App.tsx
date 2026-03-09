547b3a8da9a9fb9e7d8411785060fef0529a80b3
Bunyod <zaripovbunyod571@gmail.com>
2026-03-10 01:36:36 +0500

    fixed alies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { ButtonDemo } from "./components/Button";
+import { ButtonDemo } from "@/components/Button";
+import Logos from "@/assets/logos.png";
 
 const App = () => {
   return (
     <div>
+      <img className="w-28" src={Logos} alt="lgos" />
       <ButtonDemo />
     </div>
   );
